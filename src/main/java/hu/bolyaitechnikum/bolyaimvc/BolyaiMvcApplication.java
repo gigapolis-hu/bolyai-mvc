bf5c498dff9829c897bc4a0f4928d813b51d322d
Balázs Mátyássy <balazs.matyassy@hotmail.com>
2022-10-10 14:29:45 +0200

    Initial commit

---
+++ src/main/java/hu/bolyaitechnikum/bolyaimvc/BolyaiMvcApplication.java
@@ -0,0 +1,13 @@
+package hu.bolyaitechnikum.bolyaimvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BolyaiMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BolyaiMvcApplication.class, args);
+    }
+
+}
